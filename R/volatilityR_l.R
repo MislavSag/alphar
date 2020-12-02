7a805eb90a4feea4dbf0d04e8bea91d4fb6db550
MislavSag <mislav.sagovac@contentio.biz>
2020-12-02 16:29:54 +0100

    add params

--- R/volatilityR_l.R
+++ R/volatilityR_l.R
@@ -1,3 +1,27 @@
+library(data.table)
+library(Rcpp)
+library(purrr)
+library(ggplot2)
+library(quantmod)
+library(future.apply)
+library(anytime)
+library(PerformanceAnalytics)
+source('C:/Users/Mislav/Documents/GitHub/alphar/R/parallel_functions.R')
+source('C:/Users/Mislav/Documents/GitHub/alphar/R/outliers.R')
+source('C:/Users/Mislav/Documents/GitHub/alphar/R/import_data.R')
+source('C:/Users/Mislav/Documents/GitHub/alphar/R/execution.R')
+
+
+
+# PARAMETERS --------------------------------------------------------------
+
+
+# before backcusum function
+contract = 'SPY5'  # SPY for minute data, SPY5 for 5 minute data
+upsample = FALSE  # e.g. 10 to aggregate 5 min to 10 min data
+
+
+
 # IMPORT DATA -------------------------------------------------------------
 
 
@@ -13,12 +37,6 @@ market_data <- import_mysql(
   password = 'Theanswer0207',
   host = '91.234.46.219'
 )
-
-# market_data <- import_ib(
-#   contract = 'SPY',
-#   frequencies = '1 hour',
-#   duration = '15 Y'
-# )
 
 vix <- import_mysql(
   contract = 'VIX5',
@@ -44,3 +62,5 @@ market_data <- remove_outlier_median(market_data, median_scaler = 25)
 market_data <- merge(market_data, vix[, 'close'], join = 'left')
 colnames(market_data)[ncol(market_data)] <- 'vix'
 market_data <- na.omit(market_data)
+
+
